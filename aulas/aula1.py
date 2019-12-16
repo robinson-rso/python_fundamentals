87d9b68c5cd2eca6931ddba68ad64fb6277cb5a4
Robinson <rob.fatecano@gmail.com>
2019-12-16 14:38:08 -0200

    exercicio 1

---
+++ aulas/aula1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# shebang
+print('Questionário\n')
+nome = input("Qual é o time do coração do Pedro?  ")
+if nome == "todos":
+    print("Você acertou! :-D\n")
+else:
+    print("Você errou! :-(\n")
---
+++ exercicios/aula1ex.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+# conte quantas letras a tem no texto
+
+text = "Não deixe o barulho da opinião dos outros abafar sua voz interior. \
+E mais importante, tenha a coragem de seguir seu coração e sua intuição. \
+Eles de alguma forma já sabem o que você realmente quer se tornar. \
+Tudo o mais é secundário."
+
+# conte quantas letras a tem no texto
+print(text.count('a'))
+
+# mude todas as letras o para s
+print(text.replace('o', 's'))
+
+# divida o texto em uma lista colocando virgulas nos espaços
+print(text.split(' '))
+
